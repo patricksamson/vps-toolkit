5a05636551207cb9d3635c899226f802645b075c
Patrick Samson <patricksamson236@gmail.com>
2017-10-31 15:28:57 -0400

    Define modules list

--- config/config.php
+++ config/config.php
@@ -65,4 +65,14 @@ return [
             ],
         ],
     ],
+
+    'modules' => [
+        'BaseTools' => \App\Modules\Software\BaseTools::class,
+        'MariaDB' => \App\Modules\Software\MariaDB::class,
+        'Nginx' => \App\Modules\Software\Nginx::class,
+        'NodeJS' => \App\Modules\Software\Nodejs::class,
+        'PHP' => \App\Modules\Software\PHP::class,
+        'PostgreSQL' => \App\Modules\Software\PostgreSQL::class,
+        'Transmission' => \App\Modules\Software\Transmission::class,
+    ],
 ];
