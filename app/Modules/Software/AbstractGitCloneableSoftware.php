77ea5663383cc392b15fcca642fb42893a8bff15
Patrick Samson <patricksamson236@gmail.com>
2017-12-10 10:53:48 -0500

    Add AbstractGitCloneableSoftware

---
+++ app/Modules/Software/AbstractGitCloneableSoftware.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Modules\Software;
+
+use App\Modules\Concerns\HasDependencies;
+use App\Modules\Contracts\Installable;
+
+abstract class AbstractGitCloneableSoftware extends AbstractBaseSoftware implements Installable
+{
+    use HasDependencies;
+
+    protected $repository;
+    protected $branch = 'master';
+    protected $installPath;
+
+    public function install()
+    {
+        $this->cloneRepository();
+        $this->checkoutLatestTag();
+        $this->installDependencies();
+    }
+
+    protected function cloneRepository()
+    {
+        `git clone {$this->repository} {$this->installPath} --branch {$this->branch} --single-branch --depth 1`;
+    }
+
+    protected function checkoutLatestTag()
+    {
+        // Fetch tags from origin, with minimal commit history
+        `git fetch --depth=1 --tags {$this->installPath}`;
+
+        // Get the tag of the latest release
+        // TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+        $tag = `git describe --tags $(git rev-list --tags --max-count=1)`;
+
+        // Checkout the selected tag
+        `git checkout -f tags/{$tag} {$this->installPath}`;
+    }
+
+    protected function installDependencies()
+    {
+        `composer install -d {$this->installPath}`;
+    }
+
+    public function remove()
+    {
+        `rm -rf {$installPath}`;
+    }
+
+    abstract public function getVersion(): string;
+
+    public function isInstalled(): bool
+    {
+        return !empty(`which {$this->executable}`);
+    }
+}
