306a0df513e79e95f5aaa4553c8c4c32cf6e6db8
Patrick Samson <patricksamson236@gmail.com>
2017-12-10 10:57:15 -0500

    Edit enabled Modules

--- config/config.php
+++ config/config.php
@@ -67,7 +67,9 @@ return [
     ],
 
     'modules' => [
+        'Automysqlbackup' => \App\Modules\Software\Automysqlbackup::class,
         'BaseTools' => \App\Modules\Software\BaseTools::class,
+        'Certbot' => \App\Modules\Software\Certbot::class,
         'MariaDB' => \App\Modules\Software\MariaDB::class,
         'Nginx' => \App\Modules\Software\Nginx::class,
         'NodeJS' => \App\Modules\Software\Nodejs::class,
