490e46597f622e84af7f2af5e50d4d10f2907c09
Patrick Samson <patricksamson236@gmail.com>
2018-01-10 15:47:08 -0500

    refactor user creation

--- users/create-user.sh
+++ users/create-user.sh
@@ -1,82 +1,26 @@
-#!/bin/bash
-# Author: Patrick Samson
-# License: MIT License (refer to README.md for more details)
-#
+print_header
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-YELLOW='\e[93m'
-RED='\e[91m'
-ENDCOLOR='\033[0m'
-CYAN='\e[96m'
-GREEN='\e[92m'
-SCRIPTPATH=$(pwd)
+ask_proceed_installation "Create a new user"
 
-function pause(){
-   read -p "$*"
-}
 
-clear
-echo
-echo -e $RED
-echo -e "_    _   _ _  _ ____ ____ ____ _  _ ____ ____"
-echo -e "|     \_/  |_/  |___ | __ |___ |\ | |___ [__ "
-echo -e "|___   |   | \_ |___ |__] |___ | \| |___ ___]"
-echo -e $CYAN
-echo -e " __     ______  ____    _____           _ _    _ _   "
-echo -e " \ \   / /  _ \/ ___|  |_   _|__   ___ | | | _(_) |_ "
-echo -e "  \ \ / /| |_) \___ \    | |/ _ \ / _ \| | |/ / | __|"
-echo -e "   \ V / |  __/ ___) |   | | (_) | (_) | |   <| | |_ "
-echo -e "    \_/  |_|   |____/    |_|\___/ \___/|_|_|\_\_|\__|"
-echo
-echo -e $GREEN'Lykegenes '$SOFTNAME' Installer Script'$ENDCOLOR
+print_step_comment "Setting up username and password..."
+UNAME=''
+ask_question UNAME "Set a Username :"
+sudo useradd $UNAME --create-home --shell /bin/bash || { print_error "Creating user failed."; exit 1; }
 
-echo
-read -p 'Type y/Y and press [ENTER] to continue with the installation or any other key to exit: '
-RESP=${REPLY,,}
 
-if [ "$RESP" != "y" ]
-then
-    echo -e $RED'So you chickened out. May be you will try again later.'$ENDCOLOR
-    echo
-    pause 'Press [Enter] key to continue...'
-    cd $SCRIPTPATH
-    sudo ./setup.sh
-    exit 0
-fi
-
-echo
-
-echo -e $YELLOW'--->Setting up Username and Password...'$ENDCOLOR
-echo -n 'Set a Username and press [ENTER]: '
-read UNAME
-if [ -z "$UNAME" ]
-     then
-     echo -e '    '$RED'No Username entered.'$ENDCOLOR
-     exit 1
-fi
-sudo useradd $UNAME --create-home --shell /bin/bash || { echo -e $RED'Creating user failed.'$ENDCOLOR ; exit 1; }
-
-echo -n 'Set a Password and press [ENTER]: '
-stty -echo #hide input
-read UPASS
-stty echo #show input
-if [ -z "$UPASS" ]
-     then
-     echo -e '    '$RED'No Password entered.'$ENDCOLOR
-     exit 1
-fi
+UPASS=''
+ask_password UPASS "Set a password :"
 echo ${UNAME}:${UPASS} | chpasswd
 
-echo
-sleep 1
 
-echo -e $YELLOW'--->Configuring user...'$ENDCOLOR
-read -p 'Do you want to create a SSH key for this user? Type y/Y and press [ENTER]: '
-SSHKEY=${REPLY,,}
-if [ "$SSHKEY" = "y" ]; then
-    echo -n 'Enter your email and press [ENTER]: '
-    read EMAIL
-    sudo mkdir /home/$UNAME/.ssh
+print_step_comment "Configuring user..."
+
+
+if ask_yes_no "Do you want to create a SSH key for this user?"; then
+    EMAIL=''
+    ask_question EMAIL "Enter your email address :"
+    udo mkdir /home/$UNAME/.ssh
     ssh-keygen -t rsa -b 4096 -C "$EMAIL" -f /home/$UNAME/.ssh/id_rsa
     eval "$(ssh-agent -s)"
     ssh-add /home/$UNAME/.ssh/id_rsa
@@ -84,30 +28,22 @@ if [ "$SSHKEY" = "y" ]; then
     sudo chown -R $UNAME:$UNAME /home/$UNAME/.ssh
 fi
 
-read -p 'Add User to Sudoers? Type y/Y and press [ENTER]: '
-ANSWER=${REPLY,,}
-if [ "$ANSWER" = "y" ]; then
+
+if ask_yes_no "Add this user to Sudoers?"; then
     sudo usermod -aG sudo $UNAME
 fi
 
-read -p 'Add User to "www-data" group? Type y/Y and press [ENTER]: '
-ANSWER=${REPLY,,}
-if [ "$ANSWER" = "y" ]; then
+
+if ask_yes_no "Add this user to 'www-data' group?"; then
     sudo usermod -aG www-data $UNAME
 fi
 
-echo
-sleep 1
 
-echo -e $YELLOW'--->Reloading SSH service...'$ENDCOLOR
+print_step_comment "Reloading SSH service..."
 service ssh reload
 
-echo
-echo -e $GREEN'--->All done. '$ENDCOLOR
+print_success "All done."
 echo
 
-pause 'Press [Enter] key to continue...'
-
-cd $SCRIPTPATH
-sudo ./setup.sh
-exit 0
+pause_press_enter
+show_main_menu
