8eff9d7ee0009d67cb162ec093e3ef10793b67ea
Patrick Samson <patricksamson236@gmail.com>
2018-01-09 19:53:53 -0500

    refactor Nodejs

--- node/node-installer.sh
+++ node/node-installer.sh
@@ -3,76 +3,26 @@
 # License: MIT License (refer to README.md for more details)
 #
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-YELLOW='\e[93m'
-RED='\e[91m'
-ENDCOLOR='\033[0m'
-CYAN='\e[96m'
-GREEN='\e[92m'
-SCRIPTPATH=$(pwd)
+print_header
 
-SOFTNAME='Node.js'
+ask_proceed_installation "Node.js"
 
-function pause(){
-   read -p "$*"
-}
+# Add Node repo
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
-clear
-echo
-echo -e $RED
-echo -e "_    _   _ _  _ ____ ____ ____ _  _ ____ ____"
-echo -e "|     \_/  |_/  |___ | __ |___ |\ | |___ [__ "
-echo -e "|___   |   | \_ |___ |__] |___ | \| |___ ___]"
-echo -e $CYAN
-echo -e " __     ______  ____    _____           _ _    _ _   "
-echo -e " \ \   / /  _ \/ ___|  |_   _|__   ___ | | | _(_) |_ "
-echo -e "  \ \ / /| |_) \___ \    | |/ _ \ / _ \| | |/ / | __|"
-echo -e "   \ V / |  __/ ___) |   | | (_) | (_) | |   <| | |_ "
-echo -e "    \_/  |_|   |____/    |_|\___/ \___/|_|_|\_\_|\__|"
-echo
-echo -e $GREEN'Lykegenes '$SOFTNAME' Installer Script'$ENDCOLOR
-
-echo
-read -p 'Type y/Y and press [ENTER] to continue with the installation or any other key to exit: '
-RESP=${REPLY,,}
-
-if [ "$RESP" != "y" ]
-then
-    echo -e $RED'So you chickened out. May be you will try again later.'$ENDCOLOR
-    echo
-    pause 'Press [Enter] key to continue...'
-    cd $SCRIPTPATH
-    sudo ./setup.sh
-    exit 0
+# Install Yarn package manager
+if ! check_ppa_exists yarn; then
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 fi
 
-echo
+apt-get-update
 
-echo -e $YELLOW"--->Adding "$SOFTNAME" repository..."$ENDCOLOR
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get-install nodejs build-essential yarn
 
-echo
-sleep 1
-
-echo -e $YELLOW"--->Installing "$SOFTNAME"..."$ENDCOLOR
-sudo apt-get -y install nodejs build-essential
-
-echo -e $YELLOW"--->Updating NPM..."$ENDCOLOR
-sudo npm -g install npm@latest-2
-
-echo
-sleep 1
-
-echo -e $YELLOW"--->Installing Bower globally..."$ENDCOLOR
-sudo npm install -g bower
-
-echo
-echo -e $GREEN'--->All done. '$ENDCOLOR
+print_success "All done."
 node -v
 echo
 
-pause 'Press [Enter] key to continue...'
-
-cd $SCRIPTPATH
-sudo ./setup.sh
-exit 0
+pause_press_enter
+show_main_menu
